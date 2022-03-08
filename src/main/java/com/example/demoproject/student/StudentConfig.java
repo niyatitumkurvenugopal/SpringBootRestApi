731cda8dc5048cf67f4bb7f6413331c751f6e839
106540 <niyati.venugopal@thoughtfocous.com>
2022-03-08 14:29:53 +0530

    Added the springboot Restapi files.

---
+++ src/main/java/com/example/demoproject/student/StudentConfig.java
@@ -0,0 +1,34 @@
+package com.example.demoproject.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args->{
+             Student niyati= new Student(
+                    "Niyati",
+                    "niyati@gmail.com",
+                    LocalDate.of(1999,12,7)
+
+            );
+
+            Student gana= new Student(
+                    "Gana",
+                    "gana@gmail.com",
+                    LocalDate.of(1996,07,15)
+
+            );
+            repository.saveAll(
+                    List.of(niyati,gana)
+            );
+        };
+    }
+}
