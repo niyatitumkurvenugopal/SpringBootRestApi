d9f428ef8e3a4d2410df1dc7e2c4a015dbb29b22
106540 <niyati.venugopal@thoughtfocous.com>
2022-03-16 13:12:30 +0530

    Changing naming convention

---
+++ src/main/java/com/niyati/springbootproject/service/StudentService.java
@@ -0,0 +1,42 @@
+package com.niyati.springbootproject.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    public boolean addNewStudent(Student student) {
+        boolean existsEmail = studentRepository.selectExistsEmail(student.getEmail());
+        if (existsEmail) {
+            throw new IllegalStateException("Email " + student.getEmail() + " taken");
+        }
+        else {
+             studentRepository.save(student);
+             return true;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
