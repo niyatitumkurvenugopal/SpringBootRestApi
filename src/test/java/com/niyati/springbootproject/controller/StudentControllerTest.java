af32beb6438a2e0a0a13a11246014ba8aead86f2
106540 <niyati.venugopal@thoughtfocous.com>
2022-03-16 12:59:26 +0530

    Added StudentControllerTest class.

--- src/test/java/com/niyati/springbootproject/controller/StudentControllerTest.java
+++ src/test/java/com/niyati/springbootproject/controller/StudentControllerTest.java
@@ -1,5 +1,52 @@
 package com.niyati.springbootproject.controller;
 
-class StudentControllerTest {
+import com.niyati.springbootproject.model.Student;
+import com.niyati.springbootproject.service.StudentService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class StudentControllerTest {
+
+    @InjectMocks
+    StudentController studentController;
+
+    @Mock
+    StudentService studentService;
+
+//    @Test
+//    public void getStudentDetails() {
+//        Student student1 = new Student("Gana", "gana@gmail.com", LocalDate.of(1996, 07, 04));
+//        Student student2 = new Student("Deepthi", "deepthi@gmail.com", LocalDate.of(1995, 06, 03));
+//
+//        List<Student> students = Arrays.asList(student1, student2);
+//        List<Student> result = studentController.getStudents();
+//
+//
+//        assertThat(result).hasSize(2).contains();
+//
+//    }
+
+    @Test
+    public void addNewStudentDetails() {
+
+        String expected = "Student data is registered successfully";
+        Student student = new Student("Gana", "gana@gmail.com", LocalDate.of(1996, 07, 04));
+
+        String result = studentController.registerNewStudent(student);
+
+        assertEquals(expected, result);
+
+
+    }
 
 }
