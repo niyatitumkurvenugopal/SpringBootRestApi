955e6a51bea03e577f015b8d9036bc27bae60271
106540 <niyati.venugopal@thoughtfocous.com>
2022-03-16 09:03:50 +0530

    Changed naming convention.

--- src/test/java/com/niyati/springbootproject/springbootprojectTests.java
+++ src/test/java/com/niyati/springbootproject/springbootprojectTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class springBootProjectTests {
+class springbootprojectTests {
 
 	@Test
 	void contextLoads() {
